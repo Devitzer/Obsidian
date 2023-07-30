32311d7c8d8bd87918b50fd46e80a7bf4ec70ef8
Devitzer <defecthorizon@icloud.com>
2023-07-29 23:40:38 -0400

    Initial Commit, 1.0.0.

---
+++ main.ts
@@ -0,0 +1,36 @@
+import Parser from "./frontend/parser.ts";
+import { createGlobalEnv } from "./runtime/environment.ts";
+import { evaluate } from "./runtime/interpreter.ts";
+
+run("tests.ces");
+
+async function run(filename: string) {
+    const parser = new Parser();
+    const env = createGlobalEnv();
+
+    const input = await Deno.readTextFile(filename);
+    const program = parser.produceAST(input);
+    const result = evaluate(program, env);
+    // console.log(result);
+    // for when i need it
+}
+
+function _repl () {
+    const parser = new Parser();
+    const env = createGlobalEnv();
+
+    console.log("\nCitrineScript Repl v0.1");
+    while (true) {
+
+        const input = prompt("> ");
+        // Check for no user input or exit keyword.
+        if (!input || input.includes("exit")) {
+            Deno.exit(1);
+        }
+
+        const program = parser.produceAST(input);
+
+        const result = evaluate(program, env);
+        console.log(result);
+    }
+}
