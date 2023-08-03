0f4c7969a49e34bde158de3135d4c9189fd0ce9c
Devitzer <defecthorizon@icloud.com>
2023-08-03 19:19:24 -0400

    Accidentaly leaked a W.I.P function!

--- NativeFunctions/all.ts
+++ NativeFunctions/all.ts
@@ -2,14 +2,12 @@ import io from "./io/all.ts";
 import Date from "./Date/all.ts";
 import sys from "./sys/all.ts";
 import fs from "./fs/all.ts";
-import test from "./test/all.ts";
 
 const Native = {
     io,
     Date,
     sys,
-    fs,
-    test,
+    fs
 }
 
 export default Native;
